5b3f7de17134c3a634223da215ec236f2232ed30
Khoatbui <khoatbui.dev@gmail.com>
2019-08-03 00:06:31 +0700

    Fix coonflig

--- src/api/TravelStyleService.js
+++ src/api/TravelStyleService.js
@@ -2,26 +2,10 @@ import axios from 'axios';
 
 const TravelStyle = {
   getAllTravelStyle() {
-<<<<<<< HEAD
     return axios.get('http://103.237.144.222:3000/travelstyle/getused');
-  },
-  getTopTravelStyle() {
-    return axios.get('http://103.237.144.222:3000/travelstyle/m/getlisttravelstylepromotion');
-  },
-  getTopPromotionTravelStylePackage(){
-    return axios.get('http://103.237.144.222:3000/travelstylelist/m/getalltravelstylepromotion'); 
-  },
-  getTravelStylePackageByDestination(destinationId){
-    return axios.get('http://103.237.144.222:3000/travelstylelist/m/getalltravelstylebydestination/' + destinationId); 
-=======
-    return axios.get('http://103.237.144.222:3000/travelstyle/');
   },
   getTravelStyleById(parram) {
     return axios.get('http://103.237.144.222:3000/travelstyle/m/gettravelstylebyid/' + parram);
-  },
-  getTopTravelStyle() {
-    return axios.get('http://103.237.144.222:3000/travelstyle/m/gettop10travelstyle');
->>>>>>> 871c2fa645bd98a223218622c1e1dc7a47a4bf88
   },
 };
 
